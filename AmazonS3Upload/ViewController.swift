9f330cb535541624f2089333f67f4d95a6d65d79
Maxim Bilan <maximb.mail@gmail.com>
2016-12-18 11:42:38 +0200

    Added activity indicator

--- AmazonS3Upload/ViewController.swift
+++ AmazonS3Upload/ViewController.swift
@@ -11,15 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var uploadButton: UIButton!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 	@IBAction func uploadButtonAction(_ sender: UIButton) {
