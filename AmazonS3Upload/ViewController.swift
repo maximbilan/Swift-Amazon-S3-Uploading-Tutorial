7f786bb13747d68d31088376729f470fa52752fc
Maxim Bilan <maximb.mail@gmail.com>
2016-12-18 16:41:00 +0200

    Implemented uploading (WIP)

--- AmazonS3Upload/ViewController.swift
+++ AmazonS3Upload/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import AWSS3
+import AWSCore
+
 class ViewController: UIViewController {
 
 	@IBOutlet weak var uploadButton: UIButton!
@@ -18,7 +21,16 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func uploadButtonAction(_ sender: UIButton) {
+		let accessKey = "PLEASE_ENTER_YOUR_AMAZON_S3_ACCESS_KEY"
+		let secretKey = "PLEASE_ENTER_YOUR_AMAZON_S3_SECRET_KEY"
 		
+		let credentialsProvider = AWSStaticCredentialsProvider(accessKey: accessKey, secretKey: secretKey)
+		let configuration = AWSServiceConfiguration(region:AWSRegionType.usEast1, credentialsProvider:credentialsProvider)
+		
+		AWSServiceManager.default().defaultServiceConfiguration = configuration
+		
+		let S3BucketName = "BUCKET_NAME"
+		let remoteName = "test"
 	}
 
 }
